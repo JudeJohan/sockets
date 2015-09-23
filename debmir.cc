5660f4be7ed98b9c05996ec2a225d59907552cb3
Johan <jae91m@gmail.com>
2015-09-23 17:11:03 +0200

    m

---
+++ debmir.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "testlib.h"
+
+//inet stuffs
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+//#include <netdb.h> //cln
+
+#include <chrono>
+
+#define SRV_IP "127.0.0.1"
+
+using namespace std;
+
+int main(int narg, char* varg[]) {
+
+    return 0;
+}
